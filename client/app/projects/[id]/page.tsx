16f1eea47483d7d54b5834cbaab8d89e9a61a7de
Deshmukh P <deshmukhpurushothaman@gmail.com>
2025-02-21 13:32:49 +0530

    project header component directory change

--- client/app/projects/[id]/page.tsx
+++ client/app/projects/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { use, useState } from "react";
-import ProjectHeader from "./ProjectHeader";
+import ProjectHeader from "../ProjectHeader";
 
 type Props = {
   params: { id: string };
